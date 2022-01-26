fac95efc44b3db773828f765dd44ca844966f980
Saleban Abdillahi <01saleban@gmail.com>
2022-01-26 01:28:34 +0000

    update

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
