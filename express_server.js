5c713ceed339ffeb7c99954cec5819bc26cd73b9
Saleban Abdillahi <01saleban@gmail.com>
2022-02-10 20:45:04 +0000

    fixing express

--- express_server.js
+++ express_server.js
@@ -116,7 +116,13 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls/:shortURL", (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.longURL;
+  const shortURL = req.params.shortURL
+  const id = req.session.id
+  if (urlDatabase[shortURL].id != id){
+    const error = 'Please login.';
+    res.send(error);
+  }
+  urlDatabase[req.params.shortURL].longURL = req.body.longURL;
   res.redirect("/urls");
 });
 
@@ -137,6 +143,13 @@ app.get("/login", (req, res) => {
   };
   res.render("urls_login", templateVars);
 });
+
+app.get("/", (req, res) => {
+  if (req.session.id){
+    res.redirect("/urls");
+  } else {
+    res.redirect ("/login");
+}});
 
 app.post("/login", (req, res) => {
   let email = req.body.email;
